c41120a530e095278190746b8ca8defd73cd076d
Dominik Markowski <markowski.dominik.j@gmail.com>
2016-03-11 12:12:03 +0100

    fix tests LibraryDao and LibraryRepository

--- spring-data-jpa/src/test/java/pl/spring/demo/dao/LibraryDaoImplTest.java
+++ spring-data-jpa/src/test/java/pl/spring/demo/dao/LibraryDaoImplTest.java
@@ -3,15 +3,24 @@ package pl.spring.demo.dao;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import pl.spring.demo.entity.BookEntity;
 import pl.spring.demo.entity.LibraryEntity;
 
 import java.util.List;
 
+import javax.persistence.EntityNotFoundException;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "CommonDaoTest-context.xml")
@@ -19,6 +28,8 @@ public class LibraryDaoImplTest {
 
     @Autowired
     private LibraryDao libraryDao;
+    @Autowired
+    private BookDao bookDao;
 
     @Test
     public void testShouldFindLibraryById() {
@@ -42,4 +53,35 @@ public class LibraryDaoImplTest {
         assertFalse(librarysEntity.isEmpty());
         assertEquals("Biblioteka Miejska", librarysEntity.get(0).getName());
     }
+
+	@Test
+	@Transactional
+	@Rollback(true)
+	public void testShouldDeleteLibraryById() {
+		// given
+		final long libraryId = 0;
+		List<BookEntity> booksBeforeDelete = bookDao.findAll();
+		// when
+		libraryDao.delete(libraryId);
+		List<BookEntity> booksAfterDelete = bookDao.findAll();
+		// then
+		assertNull(libraryDao.findOne(libraryId));
+		assertNotEquals(booksBeforeDelete.size(), 0);
+		assertNotEquals(booksBeforeDelete.size(), booksAfterDelete.size());
+		assertTrue(booksAfterDelete.isEmpty());
+	}
+	
+	@Test(expected = EntityNotFoundException.class)
+	@Transactional
+	@Rollback(true)
+	public void testShouldDeleteTwiceTheSameLibraryById() {
+		// given
+		final long libraryId = 0;
+		// when
+		assertNotNull(libraryDao.findOne(libraryId));
+		libraryDao.delete(libraryId);
+		assertNull(libraryDao.findOne(libraryId));
+		libraryDao.delete(libraryId);
+		// then
+	}
 }
--- spring-data-jpa/src/test/java/pl/spring/demo/repository/LibraryRepositoryTest.java
+++ spring-data-jpa/src/test/java/pl/spring/demo/repository/LibraryRepositoryTest.java
@@ -1,11 +1,15 @@
 package pl.spring.demo.repository;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import pl.spring.demo.entity.BookEntity;
 import pl.spring.demo.entity.LibraryEntity;
@@ -23,30 +27,16 @@ public class LibraryRepositoryTest {
 	@Autowired
 	private BookRepository bookRepository;
 
-	@Test
-	public void testShouldDeleteLibraryById() {
-		// given
-		final long libraryId = 0;
-		int numberOfBooks = bookRepository.findAll().size();
-		// when
-		libraryRepository.delete(libraryId);
-		List<BookEntity> books = bookRepository.findAll();
-		// then
-		assertNull(libraryRepository.findOne(libraryId));
-		assertNotEquals(numberOfBooks, books.size());
-		assertTrue(books.isEmpty());
-	}
-	
-	@Test(expected = DataAccessException.class)
-	public void testShouldDeleteTwiceTheSameLibraryById() {
-		// given
-		final long libraryId = 0;
-		// when
-		libraryRepository.delete(libraryId);
-		assertNull(libraryRepository.findOne(libraryId));
-		libraryRepository.delete(libraryId);
-		// then
-	}
+    @Test
+    public void testShouldFindLibraryById() {
+        // given
+        final long libraryId = 0;
+        // when
+        LibraryEntity libraryEntity = libraryRepository.findOne(libraryId);
+        // then
+        assertNotNull(libraryEntity);
+        assertEquals("Biblioteka Miejska", libraryEntity.getName());
+    }
 
     @Test
     public void testShouldFindLibrarysByName() {
@@ -59,4 +49,35 @@ public class LibraryRepositoryTest {
         assertFalse(librarysEntity.isEmpty());
         assertEquals("Biblioteka Miejska", librarysEntity.get(0).getName());
     }
+    
+	@Test
+	@Transactional
+	@Rollback(true)
+	public void testShouldDeleteLibraryById() {
+		// given
+		final long libraryId = 0;
+		List<BookEntity> booksBeforeDelete = bookRepository.findAll();
+		// when
+		libraryRepository.delete(libraryId);
+		List<BookEntity> booksAfterDelete = bookRepository.findAll();
+		// then
+		assertNull(libraryRepository.findOne(libraryId));
+		assertNotEquals(booksBeforeDelete.size(), 0);
+		assertNotEquals(booksBeforeDelete.size(), booksAfterDelete.size());
+		assertTrue(booksAfterDelete.isEmpty());
+	}
+	
+	@Test(expected = DataAccessException.class)
+	@Transactional
+	@Rollback(true)
+	public void testShouldDeleteTwiceTheSameLibraryById() {
+		// given
+		final long libraryId = 0;
+		// when
+		assertNotNull(libraryRepository.findOne(libraryId));
+		libraryRepository.delete(libraryId);
+		assertNull(libraryRepository.findOne(libraryId));
+		libraryRepository.delete(libraryId);
+		// then
+	}
 }
